5d943d749a644f1f4f68457b48a06db349a10e0f
Joseph Mousa <2453578m@student.gla.ac.uk>
2021-11-22 10:11:43 +0000

    1st project

---
+++ project1/initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "JJJJJ M   M\n";
+  std::cout << "  J   MM MM\n";
+  std::cout << "  J   MM MM\n";
+  std::cout << "  J   M M M\n";
+  std::cout << "J J   M   M\n";
+  std::cout << "J J   M   M\n";
+  std::cout << " JJ   M   M\n";
+
+  return 0;
+}
